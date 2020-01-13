78e6236ded81bbccabc309de8474652fcfa272db
soldierofhell <45559276+soldierofhell@users.noreply.github.com>
2020-01-13 13:15:26 +0100

    Update track.py

--- deep_sort/sort/track.py
+++ deep_sort/sort/track.py
@@ -79,6 +79,7 @@ class Track:
 
         self._n_init = n_init
         self._max_age = max_age
+        self._match_method = ''
 
     def to_tlwh(self):
         """Get current position in bounding box format `(top left x, top left y,
@@ -123,7 +124,7 @@ class Track:
         self.age += 1
         self.time_since_update += 1
 
-    def update(self, kf, detection):
+    def update(self, kf, detection, match_method):
         """Perform Kalman filter measurement update step and update the feature
         cache.
 
@@ -143,6 +144,7 @@ class Track:
         self.time_since_update = 0
         if self.state == TrackState.Tentative and self.hits >= self._n_init:
             self.state = TrackState.Confirmed
+        self.match_method = match_method
 
     def mark_missed(self):
         """Mark this track as missed (no association at the current time step).
