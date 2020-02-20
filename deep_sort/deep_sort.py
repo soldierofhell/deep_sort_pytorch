f040d40a4014b20a860c5a81618ae2fe05283c23
soldierofhell <45559276+soldierofhell@users.noreply.github.com>
2020-02-20 13:11:15 +0100

    Update deep_sort.py

--- deep_sort/deep_sort.py
+++ deep_sort/deep_sort.py
@@ -66,7 +66,7 @@ class DeepSort(object):
             x1,y1,x2,y2 = self._tlwh_to_xyxy(box)
             track_id = track.track_id
             match_method = track.match_method
-            number = track.number
+            number = track.number if track.number is not None else -1
             outputs.append(np.array([x1,y1,x2,y2,track_id, match_method, number], dtype=np.int))
         if len(outputs) > 0:
             outputs = np.stack(outputs,axis=0)
