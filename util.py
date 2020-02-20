b94439070a35558c5a2891c46aa8b6c02871ca44
soldierofhell <45559276+soldierofhell@users.noreply.github.com>
2020-02-20 15:16:56 +0100

    Update util.py

--- util.py
+++ util.py
@@ -30,7 +30,7 @@ def draw_bbox(img, box, cls_name, identity=None, offset=(0,0)):
     return img
 
 
-def draw_bboxes(img, bbox, identities=None, match_method=None, number=None, offset=(0,0)):
+def draw_bboxes(img, bbox, identities=None, match_method=None, number=None, number_box=None, offset=(0,0)):
     for i,box in enumerate(bbox):
         x1,y1,x2,y2 = [int(i) for i in box]
         x1 += offset[0]
@@ -45,6 +45,8 @@ def draw_bboxes(img, bbox, identities=None, match_method=None, number=None, offs
         t_size = cv2.getTextSize(label, cv2.FONT_HERSHEY_PLAIN, 2 , 2)[0]
         cv2.rectangle(img,(x1, y1),(x2,y2),color,3)
         cv2.rectangle(img,(x1, y1),(x1+t_size[0]+3,y1+t_size[1]+4), color,-1)
+        if np.max(number_box)>0:
+            cv2.rectangle(img,(x1+number_box[i,0], y1number_box[i,1]),(x1+number_box[i,2], y1number_box[i,3]),color,3)
         cv2.putText(img,label,(x1,y1+t_size[1]+4), cv2.FONT_HERSHEY_PLAIN, 2, [255,255,255], 2)
     return img
 
