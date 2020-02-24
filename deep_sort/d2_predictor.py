cd5ba1483869ae91ad4cc89c8f81f30469c8dfdc
soldierofhell <45559276+soldierofhell@users.noreply.github.com>
2020-02-24 16:51:06 +0100

    Update d2_predictor.py

--- deep_sort/d2_predictor.py
+++ deep_sort/d2_predictor.py
@@ -43,39 +43,31 @@ class TensorPredictor:
         
     def _resize_shortest_edge(img):
         
-        h, w = img.size()[1:]
+        img_size = img.size()[1:]
 
-        scale = size * 1.0 / min(h, w)
-        if h < w:
-            newh, neww = size, scale * w
+        scale = self.short_edge_length * 1.0 / torch.min(img_size)
+        if img_size[0] < img_size[1]:
+            newh, neww = size, scale * img_size[1]
         else:
-            newh, neww = scale * h, size
-        if max(newh, neww) > self.max_size:
-            scale = self.max_size * 1.0 / max(newh, neww)
+            newh, neww = scale * img_size[0], size
+        if torch.max(newh, neww) > self.max_size:
+            scale = self.max_size * 1.0 / torch.max(newh, neww)
             newh = newh * scale
             neww = neww * scale
         neww = int(neww + 0.5)
         newh = int(newh + 0.5)
-        return F.interpolate(h, w, newh, neww, self.interp)
+        return F.interpolate(img, newh, neww)
 
     def __call__(self, original_image):
         """
-        Args:
-            original_image (np.ndarray): an image of shape (H, W, C) (in BGR order).
-        Returns:
-            predictions (dict):
-                the output of the model for one image only.
-                See :doc:`/tutorials/models` for details about the format.
         """
-        with torch.no_grad():  # https://github.com/sphinx-doc/sphinx/issues/4258
+        with torch.no_grad():
             # Apply pre-processing to image.
             if self.input_format == "RGB":
                 # whether the model expects BGR inputs or RGB
                 original_image = original_image[:, :, ::-1]
             height, width = original_image.shape[:2]
-            image = self.transform_gen.get_transform(original_image).apply_image(original_image)
-            image = torch.as_tensor(image.astype("float32").transpose(2, 0, 1))
-
+            image = _resize_shortest_edge(original_image)
             inputs = {"image": image, "height": height, "width": width}
             predictions = self.model([inputs])[0]
             return predictions
