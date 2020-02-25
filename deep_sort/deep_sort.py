1542ecc1261ca022b3e05894b22560297ac28ba7
soldierofhell <45559276+soldierofhell@users.noreply.github.com>
2020-02-25 16:56:02 +0100

    Update deep_sort.py

--- deep_sort/deep_sort.py
+++ deep_sort/deep_sort.py
@@ -194,6 +194,7 @@ class DeepSort(object):
                 number_box = number_instance.pred_boxes.tensor[0].detach().cpu().numpy().astype(int)
                 padded_box = self._padded_bbox(number_box, player_crop.shape[0], player_crop.shape[1])     
                 print('player crop: ', player_crop.size())
+                print('padded_box: ', padded_box)
                 number_crop = player_crop[:, padded_box[1]:padded_box[3], padded_box[0]:padded_box[2]]
 
                 pred, confidence_score = self.number_decoder.predict(number_crop, input_size=(100, 32), dictionary=self.team_numbers[team_id])
