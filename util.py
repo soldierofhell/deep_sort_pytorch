7a1d623f940b7b05f7904761040066996eb6e6ef
soldierofhell <45559276+soldierofhell@users.noreply.github.com>
2020-03-03 23:12:18 +0100

    Update util.py

--- util.py
+++ util.py
@@ -42,7 +42,7 @@ def draw_bboxes(frame_id, img, bbox, identities=None, match_method=None, number=
         id = int(identities[i]) if identities is not None else 0    
         color = COLORS_10[id%len(COLORS_10)]
         match_dict = {0: 'N', 1: 'F', 2: 'I'}  
-        label = '{}{:d}{}{}{}'.format("", id, number[i], detection_id[i], min_cost[i]) # match_dict[match_method[i]]
+        label = '{}{:d}|{}|{}|{}'.format("", id, number[i], detection_id[i], min_cost[i]) # match_dict[match_method[i]]
         t_size = cv2.getTextSize(label, cv2.FONT_HERSHEY_PLAIN, 2 , 2)[0]
         cv2.rectangle(img,(x1, y1),(x2,y2),color,3)
         cv2.rectangle(img,(x1, y1),(x1+t_size[0]+3,y1+t_size[1]+4), color,-1)
