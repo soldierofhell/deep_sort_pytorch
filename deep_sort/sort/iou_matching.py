401f8d06c66e42794d91c4a7456931b65eacfdd3
soldierofhell <45559276+soldierofhell@users.noreply.github.com>
2020-03-03 09:47:37 +0100

    Update iou_matching.py

--- deep_sort/sort/iou_matching.py
+++ deep_sort/sort/iou_matching.py
@@ -74,9 +74,9 @@ def iou_cost(tracks, detections, track_indices=None,
 
     cost_matrix = np.zeros((len(track_indices), len(detection_indices)))
     for row, track_idx in enumerate(track_indices):
-        if tracks[track_idx].time_since_update > 1:
-            cost_matrix[row, :] = linear_assignment.INFTY_COST
-            continue
+        #if tracks[track_idx].time_since_update > 1:
+        #    cost_matrix[row, :] = linear_assignment.INFTY_COST
+        #    continue
 
         bbox = tracks[track_idx].to_tlwh()
         candidates = np.asarray([detections[i].tlwh for i in detection_indices])
