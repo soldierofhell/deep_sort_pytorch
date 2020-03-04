15cff511383484752283c64ed612af18996a62f0
soldierofhell <45559276+soldierofhell@users.noreply.github.com>
2020-03-04 16:42:50 +0100

    Update util.py

--- util.py
+++ util.py
@@ -46,10 +46,10 @@ def draw_bboxes(frame_id, new_sequence, img, bbox, identities=None, match_method
         label = '{}{:d}|{}|{}|{}'.format("", id, number[i], detection_id[i], min_cost[i]) # match_dict[match_method[i]]
         t_size = cv2.getTextSize(label, cv2.FONT_HERSHEY_PLAIN, 2 , 2)[0]
         cv2.rectangle(img,(x1, y1),(x2,y2),color,3)
-        cv2.rectangle(img,(x1, y1),(x1+t_size[0]+3,y1+t_size[1]+4), color,-1)
-        if np.max(number_box)>0:
-            cv2.rectangle(img,(x1+number_box[i,0], y1 + number_box[i,1]),(x1+number_box[i,2], y1 + number_box[i,3]),color,3)
-        cv2.putText(img,label,(x1,y1+t_size[1]+4), cv2.FONT_HERSHEY_PLAIN, 2, [255,255,255], 2)
+        #cv2.rectangle(img,(x1, y1),(x1+t_size[0]+3,y1+t_size[1]+4), color,-1)
+        #if np.max(number_box)>0:
+        #    cv2.rectangle(img,(x1+number_box[i,0], y1 + number_box[i,1]),(x1+number_box[i,2], y1 + number_box[i,3]),color,3)
+        #cv2.putText(img,label,(x1,y1+t_size[1]+4), cv2.FONT_HERSHEY_PLAIN, 2, [255,255,255], 2)
     return img
 
 def softmax(x):
