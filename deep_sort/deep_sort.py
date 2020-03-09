4d832f91600883ff40b22ad48349b19f7096fbfa
soldierofhell <45559276+soldierofhell@users.noreply.github.com>
2020-03-09 16:35:34 +0100

    Update deep_sort.py

--- deep_sort/deep_sort.py
+++ deep_sort/deep_sort.py
@@ -81,7 +81,10 @@ class DeepSort(object):
     def update(self, bbox_xywh, confidences, ori_img, new_sequence):
         self.height, self.width = ori_img.shape[:2]
         # generate detections
-        features = self._get_features(bbox_xywh, ori_img)
+        if self.extractor == 'pedestrian':
+            features = self._get_features(bbox_xywh, ori_img)
+        # else in _predict_numbers(), TODO: change this name           
+            
         numbers, team_ids = self._predict_numbers(bbox_xywh, ori_img)
         
         bbox_tlwh = self._xywh_to_tlwh(bbox_xywh)
