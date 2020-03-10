1d4f08eabb50ff37ae6afa96cbb92542f0319348
soldierofhell <45559276+soldierofhell@users.noreply.github.com>
2020-03-10 11:13:04 +0100

    Update tracker.py

--- deep_sort/sort/tracker.py
+++ deep_sort/sort/tracker.py
@@ -205,7 +205,7 @@ class Tracker:
         mean, covariance = self.kf.initiate(detection.to_xyah())
         self.tracks.append(Track(
             mean, covariance, self._next_id, self.n_init, self.max_age,
-            detection.feature, detection_id=detection_idx, sequence_no=self.sequence_no))
+            detection.feature, detection_id=detection_idx, sequence_no=self.sequence_no, detection=detection))
         self._next_id += 1
         
     def _match_number(self, track_id):
