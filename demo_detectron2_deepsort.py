3904a0b0f9811d4b8c34bf8ab6ca7507a1ada5fb
soldierofhell <45559276+soldierofhell@users.noreply.github.com>
2020-03-10 10:24:24 +0100

    Update demo_detectron2_deepsort.py

--- demo_detectron2_deepsort.py
+++ demo_detectron2_deepsort.py
@@ -194,7 +194,8 @@ class Detector(object):
 
                 #bbox_xcycwh[:, 3:] *= 1.2
 
-                outputs, detections = self.deepsort.update(bbox_xcycwh, cls_conf, im, new_sequence)
+                outputs, detections = self.deepsort.update(bbox_xcycwh, cls_conf, im, new_sequence, frame_id)
+                self.deepsort.export('/content/export.json')
                 if len(outputs) > 0:                    
                     bbox_xyxy = outputs[:, :4]
                     #dh = ((0.1/1.2)*(bbox_xyxy[:,3]-bbox_xyxy[:,1])).astype(int)
