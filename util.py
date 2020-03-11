ec7c7fe45fe9d00765d10caa49ac514de41d9c1a
soldierofhell <45559276+soldierofhell@users.noreply.github.com>
2020-03-11 12:31:46 +0100

    Update util.py

--- util.py
+++ util.py
@@ -102,18 +102,18 @@ def draw_offline(img_dir, track_json, sequence_json, config_yml):
                     x1,y1,x2,y2 = track[config['bbox']['type']]            
                     cv2.rectangle(img,(x1, y1),(x2,y2),color,3)
                         
-            if config['flags'].getboolean('number'):
-                label = str(track['number'])
-                print(label)
-                t_size = cv2.getTextSize(label, cv2.FONT_HERSHEY_PLAIN, 2 , 2)[0]
-                cv2.putText(img,label,(x1,y1+t_size[1]+4), cv2.FONT_HERSHEY_PLAIN, 2, [255,255,255], 2)
+                    if config['flags'].getboolean('number'):
+                        label = str(track['number'])
+                        print(label)
+                        t_size = cv2.getTextSize(label, cv2.FONT_HERSHEY_PLAIN, 2 , 2)[0]
+                        cv2.putText(img,label,(x1,y1+t_size[1]+4), cv2.FONT_HERSHEY_PLAIN, 2, [255,255,255], 2)
                         
                         
-            if config['flags'].getboolean('track_id'):
-                label = str(track['track_id'])
-                print(label)
-                t_size = cv2.getTextSize(label, cv2.FONT_HERSHEY_PLAIN, 2 , 2)[0]
-                cv2.putText(img,label,(x1,y1+t_size[1]+4), cv2.FONT_HERSHEY_PLAIN, 2, [255,255,255], 2)
+                    if config['flags'].getboolean('track_id'):
+                        label = str(track['track_id'])
+                        print(label)
+                        t_size = cv2.getTextSize(label, cv2.FONT_HERSHEY_PLAIN, 2 , 2)[0]
+                        cv2.putText(img,label,(x1,y1+t_size[1]+4), cv2.FONT_HERSHEY_PLAIN, 2, [255,255,255], 2)
                 
                         
                 
