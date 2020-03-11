f2a00b07b1fd4885b1eaba2876197b9403a9e87b
soldierofhell <45559276+soldierofhell@users.noreply.github.com>
2020-03-11 12:24:59 +0100

    Update util.py

--- util.py
+++ util.py
@@ -88,7 +88,10 @@ def draw_offline(img_dir, track_json, sequence_json, config_yml):
 
         for track in tracks:            
             track_id = track['track_id']            
-            color = COLORS_10[track_id%len(COLORS_10)] # todo: 
+            color = COLORS_10[track_id%len(COLORS_10)] # todo:
+            
+            if config['flags'].getboolean('frame_id'):
+                cv2.putText(img, f'FRAME_ID: {frame_id}', (0, 50), cv2.FONT_HERSHEY_PLAIN, 3, [255,255,255], 2)
             
             if config['flags'].getboolean('player_box'):
                 print(track[config['bbox']['type']])
