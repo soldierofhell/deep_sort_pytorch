1515949abfc51910f5134cb3c00059c7761ff7d3
soldierofhell <45559276+soldierofhell@users.noreply.github.com>
2020-03-11 10:00:33 +0100

    Update util.py

--- util.py
+++ util.py
@@ -92,7 +92,7 @@ def draw_offline(img_dir, track_json, sequence_json, config_yml):
                 cv2.rectangle(img,(x1, y1),(x2,y2),color,3)
                 
         if config['output']['video']:
-            vw.write(draw_im)
+            vw.write(img)
 
 
 if __name__ == '__main__':
