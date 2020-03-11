866266d5141531ecce556094e6ecd0908d28acab
soldierofhell <45559276+soldierofhell@users.noreply.github.com>
2020-03-11 10:50:12 +0100

    Update util.py

--- util.py
+++ util.py
@@ -89,6 +89,7 @@ def draw_offline(img_dir, track_json, sequence_json, config_yml):
             color = COLORS_10[track_id%len(COLORS_10)] # todo: 
             
             if config['flags']['player_box']:
+                print(track[config['bbox']['type']])
                 x1,y1,x2,y2 = track[config['bbox']['type']]            
                 cv2.rectangle(img,(x1, y1),(x2,y2),color,3)
                 
