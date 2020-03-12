99238d7112141fe41d5f4e7e8eeae1c4de71b506
soldierofhell <45559276+soldierofhell@users.noreply.github.com>
2020-03-12 11:55:31 +0100

    Update util.py

--- util.py
+++ util.py
@@ -107,7 +107,7 @@ def draw_offline(img_dir, track_json, detection_json, sequence_json, config_yml)
                     x1,y1,x2,y2 = detection['bbox']            
                     cv2.rectangle(img, (x1,y1), (x2,y2), color, 3)            
                     if config['flags'].getboolean('detection_id'):
-                        label = str(detection_id)
+                        label = f'{detection_id}:{detection['confidence']:.2f}' 
                         t_size = cv2.getTextSize(label, cv2.FONT_HERSHEY_PLAIN, 2 , 2)[0]
                         cv2.putText(img,label,(x1,y1+t_size[1]+4), cv2.FONT_HERSHEY_PLAIN, 2, [255,255,255], 2)
                     
