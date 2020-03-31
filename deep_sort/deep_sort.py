01866c6b22a26471057e33104035f31a66cd803a
soldierofhell <45559276+soldierofhell@users.noreply.github.com>
2020-04-01 00:32:13 +0200

    Update deep_sort.py

--- deep_sort/deep_sort.py
+++ deep_sort/deep_sort.py
@@ -577,6 +577,7 @@ class DeepSort(object):
                 'number_bbox': track.number_bbox if track.number_bbox is not None else [0,0,0,0],
                 'min_cost': track.min_cost,
                 'detection_conf': track.detection.confidence,
+                'detection_coordinates': track.detection.coordinates.tolist(),
                 'team_id': track.team_id,
             }
             track_list.append(track_dict)           
