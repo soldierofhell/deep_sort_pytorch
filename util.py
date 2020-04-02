91375b3fbe95963b9a814c3d068d6fc74f522acb
soldierofhell <45559276+soldierofhell@users.noreply.github.com>
2020-04-02 15:06:34 +0200

    Update util.py

--- util.py
+++ util.py
@@ -69,7 +69,8 @@ import os
 
 def _get_color(config, track):
     if config['flags'].getboolean('team_color'):
-        color = tuple(map(config['teams'][f"team{track['team_id']}_color"].split(','), int))
+        team_id = track['team_id']
+        color = tuple(map(config['teams'][f'team{team_id}_color'].split(','), int))
     else:
         color = COLORS_10[track['track_id']%len(COLORS_10)]
     return color
